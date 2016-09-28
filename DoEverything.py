7a435e0cdad42638c8b6d6dca21bde3085feef4b
Charles Whaples <whaples2011@gmail.com>
2016-09-28 13:07:22 -0400

    TODO's and duration calculation

--- DoEverything.py
+++ DoEverything.py
@@ -27,6 +27,20 @@ except EOFError as eofe:
     sys.stderr.write('EOFError on file ' + wav_filename + '\n' + \
                      str(eofe) + '. Skipping.\n')
 
+#frames
+frames = wf.getnframes()
+# Framerate
+rate = wf.getframerate()
+
+duration = frames/rate
+print(frames)
+print(duration)
+
+#TODO: Looks like doing one light call per read of the buffer will give us ~40 calls
+#TODO: Realisticly we should only be doing 30 calls in the course of 3 seconds (10 calls per light per second)
+#TODO: Things to test, how does the playback respond when we're making 3 calls each loop of the buffer, do we get sound delay?
+#TODO: If we do get sound delay, how does it respond to one light being called once per loop?
+
 
 # Instantiate PyAudio.
 p = pyaudio.PyAudio()
@@ -66,8 +80,8 @@ while len(data) > 0:
         plot_list.append(diff)
 
 
-    # plt.plot(range(0, len(plot_list)), plot_list)
-    # plt.show()
+    plt.plot(range(0, len(plot_list)), plot_list)
+    plt.show()
 
     # plt.plot(range(0, len(diff_list)), diff_list)
     # plt.show()
